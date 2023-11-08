ff341dc8b9f1933e4d0123fb060622d927198ed9
Denis Svetleishii <denissvetleishii@outlook.com>
2023-11-08 13:03:32 +0400

    feat: add solution isNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -450,10 +450,9 @@ function getNumberValue(number) {
  * 5        => true
  * '5'      => false
  */
-function isNumber(/* number */) {
-  throw new Error('Not implemented');
+function isNumber(number) {
+  return typeof number === 'number' && Number.isFinite(number);
 }
-
 /**
  * Returns a boolean value indicating whether a number is an integer or not.
  *
